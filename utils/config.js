d51b521316b30920a5cf808ffbd11edd6de56d67
jeetesh kumar shaw <jeeteshshaw@gmail.com>
2023-05-18 18:52:18 +0530

    live server

--- utils/config.js
+++ utils/config.js
@@ -1,2 +1,3 @@
 // export const baseUrl = "https://quill-server-app.herokuapp.com/"
-export const baseUrl = "http://192.168.0.6:8000/"
+// export const baseUrl = "http://192.168.0.6:8000/"
+export const baseUrl = "http://4j3f.l.serverhost.name:8000/"
